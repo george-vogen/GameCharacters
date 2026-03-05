8523f37bebdadc7c236a8f666f17932694dbca41
George Vogen <gvogen@my.wctc.edu>
2026-03-05 15:38:35 -0600

    [1.00] Created classes for dk/sf2 characters

---
+++ DonkeyKong.cs
@@ -0,0 +1,9 @@
+public class DonkeyKong : Character
+{
+    public string? Species { get; set; }
+
+    public override string Display()
+    {
+        return $"Id: {Id}\nSpecies: {Species}\nName: {Name}\nDescription: {Description}";
+    }
+}
